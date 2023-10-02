6ebb9947236a623efa74f53d42b2ebdfad0002b7
JoseCantonMoreno <josecantonmoreno48@gmail.com>
2023-10-02 17:27:49 +0200

    Ejercicio 1 Desarrollo Servidor

---
+++ Desktop/Ejercicio1.php
@@ -0,0 +1,678 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+    <script src="Ejercicio1.js"></script>
+    <style>
+        body {
+    background-color: lightskyblue;
+}
+h1 {
+    text-align: center;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.Ejercicio1 {
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio1:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Ejercicio2 {
+    margin-top: 10px;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio2:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Ejercicio3 {
+    margin-top: 10px;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio3:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Ejercicio4 {
+    margin-top: 10px;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio4:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Ejercicio5 {
+    margin-top: 10px;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio5:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Ejercicio6 {
+    margin-top: 10px;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio6:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Ejercicio7 {
+    margin-top: 10px;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio7:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Ejercicio8 {
+    margin-top: 10px;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio8:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Ejercicio9 {
+    margin-top: 10px;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    text-align: center;
+}
+.Ejercicio9:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.todo {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+hr {
+    border: 1px solid black;
+    margin-top: 0%;
+    margin-bottom: 0%;
+}
+.Ejercicio1A:hover {
+    background-color: lightcoral;
+}
+.Ejercicio1B:hover {
+    background-color: lightcoral;
+}
+.Ejercicio1C:hover {
+    background-color: lightcoral;
+}
+.Ejercicio1D:hover {
+    background-color: lightcoral;
+}
+.Reducir {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Mensaje {
+    display: none;
+}
+p {
+    text-align: center;
+    font-size: larger;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.ejercicio2B {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.ejercicio3b {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.ejercicio4B {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.ejercicio5B {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.ejercicio6B {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.ejercicio7B {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.ejercicio8B {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.ejercicio9B {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.Reducir2 {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir2:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Reducir3 {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir3:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Reducir4 {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir4:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Reducir5 {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir5:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Reducir6 {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir6:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Reducir7 {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir7:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Reducir8 {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir8:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Reducir9 {
+    display: none;
+    background-color: burlywood;
+    border: 3px solid black;
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+}
+.Reducir9:hover {
+    background-color: lightcoral;
+    border: 3px solid black;
+    width: 200px;
+}
+.Contenedor:hover {
+background-color: lightcoral;
+}
+    </style>
+    <script>    function Ejercicio1(){
+    var elemento = document.getElementsByClassName("Todo")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir")[0];
+    elemento4.style.display = "block";
+    var elemento5 = document.getElementsByClassName("Mensaje")[0];
+    elemento5.style.display = "block";
+    }
+    function compEjercicio1(){
+    var elemento2 = document.getElementsByClassName("Todo")[0];
+    elemento2.style.display = "none";
+    var elemento3 = document.getElementsByClassName("Reducir")[0];
+    elemento3.style.display = "none";
+    var elemento6 = document.getElementsByClassName("Mensaje")[0];
+    elemento6.style.display = "none"; 
+    }
+    function Ejercicio2(){
+    var elemento = document.getElementsByClassName("ejercicio2B")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir2")[0];
+    elemento4.style.display = "block";
+    }
+    function compEjercicio2(){
+    var elemento = document.getElementsByClassName("ejercicio2B")[0];
+    elemento.style.display = "none"; 
+    var elemento4 = document.getElementsByClassName("Reducir2")[0];
+    elemento4.style.display = "none";
+    }
+    function Ejercicio3(){
+    var elemento = document.getElementsByClassName("ejercicio3b")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir3")[0];
+    elemento4.style.display = "block";
+    }
+    function compEjercicio3(){
+    var elemento = document.getElementsByClassName("ejercicio3b")[0];
+    elemento.style.display = "none"; 
+    var elemento4 = document.getElementsByClassName("Reducir3")[0];
+    elemento4.style.display = "none";
+    }
+    function Ejercicio4(){
+    var elemento = document.getElementsByClassName("ejercicio4B")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir4")[0];
+    elemento4.style.display = "block";
+    }
+    function compEjercicio4(){
+    var elemento = document.getElementsByClassName("ejercicio4B")[0];
+    elemento.style.display = "none"; 
+    var elemento4 = document.getElementsByClassName("Reducir4")[0];
+    elemento4.style.display = "none";
+    }
+    function Ejercicio5(){
+    var elemento = document.getElementsByClassName("ejercicio5B")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir5")[0];
+    elemento4.style.display = "block";
+    }
+    function compEjercicio5(){
+    var elemento = document.getElementsByClassName("ejercicio5B")[0];
+    elemento.style.display = "none"; 
+    var elemento4 = document.getElementsByClassName("Reducir5")[0];
+    elemento4.style.display = "none";
+    }
+    function Ejercicio6(){
+    var elemento = document.getElementsByClassName("ejercicio6B")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir6")[0];
+    elemento4.style.display = "block";
+    }
+    function compEjercicio6(){
+    var elemento = document.getElementsByClassName("ejercicio6B")[0];
+    elemento.style.display = "none"; 
+    var elemento4 = document.getElementsByClassName("Reducir6")[0];
+    elemento4.style.display = "none";
+    }
+    function Ejercicio7(){
+    var elemento = document.getElementsByClassName("ejercicio7B")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir7")[0];
+    elemento4.style.display = "block";
+    }
+    function compEjercicio7(){
+    var elemento = document.getElementsByClassName("ejercicio7B")[0];
+    elemento.style.display = "none"; 
+    var elemento4 = document.getElementsByClassName("Reducir7")[0];
+    elemento4.style.display = "none";
+    }
+    function Ejercicio8(){
+    var elemento = document.getElementsByClassName("ejercicio8B")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir8")[0];
+    elemento4.style.display = "block";
+    }
+    function compEjercicio8(){
+    var elemento = document.getElementsByClassName("ejercicio8B")[0];
+    elemento.style.display = "none"; 
+    var elemento4 = document.getElementsByClassName("Reducir8")[0];
+    elemento4.style.display = "none";
+    }
+    function Ejercicio9(){
+    var elemento = document.getElementsByClassName("ejercicio9b")[0];
+    elemento.style.display = "block"; 
+    var elemento4 = document.getElementsByClassName("Reducir9")[0];
+    elemento4.style.display = "block";
+    }
+    function compEjercicio9(){
+    var elemento = document.getElementsByClassName("ejercicio9b")[0];
+    elemento.style.display = "none"; 
+    var elemento4 = document.getElementsByClassName("Reducir9")[0];
+    elemento4.style.display = "none";
+    }</script>
+</head>
+<body>
+    <h1>Desarrollo Web Servidor Primera Practica</h1>
+
+    <div class="Ejercicio1" onclick="Ejercicio1()">
+        Ejercicio 1
+    </div>
+
+    <div class="Reducir" onclick="compEjercicio1()">
+        Reducir
+    </div>
+
+    <div class="Ejercicio2" onclick="Ejercicio2()">
+        Ejercicio 2
+    </div>
+    
+    <div class="Reducir2" onclick="compEjercicio2()">
+        Reducir
+    </div>
+    <div class="Ejercicio3" onclick="Ejercicio3()">
+        Ejercicio 3
+    </div>
+    
+    <div class="Reducir3" onclick="compEjercicio3()">
+        Reducir
+    </div>
+    <div class="Ejercicio4" onclick="Ejercicio4()">
+        Ejercicio 4
+    </div>
+    
+    <div class="Reducir4" onclick="compEjercicio4()">
+        Reducir
+    </div>
+    <div class="Ejercicio5" onclick="Ejercicio5()">
+        Ejercicio 5
+    </div>
+    
+    <div class="Reducir5" onclick="compEjercicio5()">
+        Reducir
+    </div>
+    <div class="Ejercicio6" onclick="Ejercicio6()">
+        Ejercicio 6
+    </div>
+    
+    <div class="Reducir6" onclick="compEjercicio6()">
+        Reducir
+    </div>
+    <div class="Ejercicio7" onclick="Ejercicio7()">
+        Ejercicio 7
+    </div>
+    
+    <div class="Reducir7" onclick="compEjercicio7()">
+        Reducir
+    </div>
+    <div class="Ejercicio8" onclick="Ejercicio8()">
+        Ejercicio 8
+    </div>
+    
+    <div class="Reducir8" onclick="compEjercicio8()">
+        Reducir
+    </div>
+    <div class="Ejercicio9" onclick="Ejercicio9()">
+        Ejercicio 9
+    </div>
+    
+    <div class="Reducir9" onclick="compEjercicio9()">
+        Reducir
+    </div>
+    <div class="Mensaje">
+        <p><b>El valor Booleano si esta en "True" saldria un (1) pero si el valor Booleano esta en "False" no saldria nada.</b></p>
+    </div>
+    <div class="Todo">
+        <div class="Ejercicio1A">
+            <?php
+                $texto2 = "Mundo<br>";
+                $texto1 = "Hola";
+                $texto3 = " ";
+                $Concatenado = $texto1.$texto3.$texto2;
+                echo "<b>"."1 ".$Concatenado."</b>";
+            ?>
+        </div>
+        <hr>
+        <div class="Ejercicio1B">
+            <?php
+                $Booleano = true;
+                if ($Booleano == 1) {
+                    echo "<b>"."1B "."El booleano es Verdadero (".$Booleano.")"."<br></b>"; 
+                } else {
+                    echo "<b>"."1B "."El booleane es falso (Nada)"."<br></b>";
+                }
+            ?>
+        </div>
+        <hr>
+        <div class="Ejercicio1C">
+            <?php
+                define("PI", 3.14);
+                echo "<b>"."1C ".PI."<br></b>";
+            ?>
+        </div>
+        <hr>
+        <div class="Ejercicio1D">
+            <?php
+                $Array1 = array(
+                    "valor1" => "1",
+                    "valor2" => "2",
+                    "valor3" => "3",
+                );
+                echo "<b>"."1D ";
+                print_r($Array1);
+            ?>
+        </div>
+    </div>
+    <div class="ejercicio2B">
+        <div class="Contenedor">
+            <?php
+                $Booleano = false;
+                if ($Booleano == 1) {
+                    echo "<b>"."1B "."El booleano es Verdadero (".$Booleano.")"."<br></b>"; 
+                } else {
+                    echo "<b>"."1B "."El booleane es falso (Nada)"."<br></b>";
+                }
+            ?>
+        </div>
+    </div>
+    <div class="ejercicio3B">
+        <div class="Contenedor">
+            <?php
+                $texto2 = "Mundo<br>";
+                $texto1 = "Hola";
+                $texto3 = " ";
+                $Concatenado = $texto1.$texto3.$texto2;
+                $concatenar = str_replace(" ", "", $Concatenado); //funcion para eliminar caracter especificado
+                echo $concatenar;
+            ?>
+        </div>
+    </div>
+    <div class="ejercicio4B">
+        <div class="Contenedor">
+            <?php
+                echo "El operador \"+\" sirve para sumar el valor de variables. Con la \"/\" podemos
+                dividir valores entre variables. El símbolo del dólar \"$\" indica que estamos
+                utilizando variables pero no lo usaremos en las constantes o globales";
+            ?>
+        </div>
+    </div>
+    <div class="ejercicio5B">
+        <div class="Contenedor">
+            <?php
+                $Texto="El operador \"+\" sirve para sumar el valor de variables. Con la \"/\" podemos
+                dividir valores entre variables. El símbolo del dólar \"$\" indica que estamos
+                utilizando variables pero no lo usaremos en las constantes o globales";
+                $longitud=strlen($Texto);
+                echo "El parrafo tiene ".$longitud." caracteres.";
+            ?>
+        </div>
+    </div>
+    <div class="ejercicio6B">
+        <div class="Contenedor">
+            <?php
+                $Frase="Hello"." "."World";
+                $ArrayVoc = array('a','e','i','o','u');
+                $Reemplazar=str_replace($ArrayVoc,"",$Frase);
+                echo $Reemplazar;
+            ?>
+        </div>
+    </div>
+    <div class="ejercicio7B">
+        <div class="Contenedor">
+            <?php
+                $Funcion=null;
+                $longitud3=strlen($Funcion);
+                if ($Funcion==null) {
+                    echo "La Funcion esta vacia";
+                } else if($longitud3>0) {
+                    echo "La funcion no esta vacia";
+                }
+            ?>
+        </div>
+    </div>
+    <div class="ejercicio8B">
+        <div class="Contenedor">
+            <?php
+            $Frase=intval($Frase);
+            echo $Frase."<br>";
+            echo "No sale nada porque no se puede pasar un String a Entero";
+            ?>
+        </div>
+    </div>
+    <div class="ejercicio9b">
+        <div class="Contenedor">
+            <?php
+            $Raiz = sqrt(144);
+            echo "9A---> ".$Raiz."<br>";
+            $Potencia = pow(2,8);
+            echo "9B---> ".$Potencia."<br>";
+            $Resto = 100%6;
+            echo "9C---> ".$Resto."<br>";
+            function MCD($valor1, $valor2) {
+                if ($valor1 == 0) {
+                    return $valor2;
+                }
+                return MCD($valor2%$valor1, $valor1);
+            }
+            echo "9D---> ".MCD(3,6);
+            ?>
+        </div>
+    </div>
+</body>
+</html>
